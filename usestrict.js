db35a71e632838a8fd1d2dc16bbb99e346b36abf
117Castro <131223103+117Castro@users.noreply.github.com>
2024-12-22 15:34:50 -0600

    creacion de directiroios estudio.html y usestrict.js
    cree los nuevos directorios para practicas en el js code

---
+++ usestrict.js
@@ -0,0 +1,27 @@
+
+
+//pos no hay efecto en el agregar esta linea de modo estricto
+
+'use strict';
+alert(23+43);
+console.log("prueba del uso de scrit");
+
+function suma(){
+    
+
+    console.log(23 + 34- 98 * 3 );// vkughfvk 
+
+    return;
+}
+
+suma();
+
+
+
+function muesrtauser(){
+
+    let userpron= prompt("como te llamas?");
+
+    alert(userpron);
+}
+muesrtauser();
