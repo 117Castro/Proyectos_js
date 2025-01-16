e34b1dba620b4d3bfd962b0898ceeeedf994b32b
117Castro <131223103+117Castro@users.noreply.github.com>
2025-01-16 11:48:29 -0600

    comprendiendo el funcionamiento de metodologia de pilas y colas en calidad.ja
    aprendiendo nueas funciones para la interaccion de indices en los arreglos.

--- practicas_/calidad.js
+++ practicas_/calidad.js
@@ -30,7 +30,52 @@ console.log(cosas[0]);
 console.log(cosas[4]);
 console.log(cosas[5]);
 
+console.log("Pds. los datos se modifican en todo el arreglo y modifica todos los console.log()")
 console.log(cosas);// mostrara el arreglo por completo...
+
+/** solo por molestar pq estaba y no sabia que pedo */
+
+let myArray_frutas = [["manzana", 2], ["perejil", 5], ["marihuana", 89]];
+console.log(myArray_frutas); //impimira todo el arreglo con los indices proporcionados...
+
+myArray_frutas.pop(); //me permite eliminar el ultimo dato de mi arreglo 
+console.log(myArray_frutas);
+
+
+let show_old_date = myArray_frutas.shift(); //usando el comando de .shift puedo eliminar el ultimo dato de mi arreglo y almacenarlo en una variable.
+console.log(myArray_frutas);
+console.log(show_old_date);
+
+//usando el nuevo comando de .unshift agrego un nuevo dato pero al inicio de mi arreglo...
+myArray_frutas.unshift(["nuevo dato 2d", 234]); 
+console.log(myArray_frutas);
+
+
+
+/**Usando la metodologia de cola (queue) utilizamos las funciones de .push() y .shift()
+ * 
+ * usemos el ejemplo de una cola de supepr mercados. 
+ */
+Array.push()
+Array.shift();  
+
+
+/** Usando la metodologia de pila usamos .push() y .pop() para manejar los datos 
+ * 
+ * usemos el ejemplo de los platos cuando los estamos lavando
+ */
+
+Array.push();
+Array.pop();
+
+/** Ahora usando el otras funcines: */
+
+Array.unshift(); //esta funcion me permite integrar elementos al inicio de los arreglos. 
+
+
+
+
+
 
 
 
