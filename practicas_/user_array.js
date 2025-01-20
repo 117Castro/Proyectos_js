511abd9f959011d7a499b83bd7f7ed287a6bc96e
117Castro <131223103+117Castro@users.noreply.github.com>
2025-01-19 21:14:58 -0600

    implementacion de nuevas funciones en el proyecto
    implementacion del metodo pila en el proyecto y modificacion de metodo cola. tambien creacion de nueva funcion shift().

--- practicas_/user_array.js
+++ practicas_/user_array.js
@@ -1,16 +1,32 @@
-
-
 /**funcion que toma un numero del usuario y lo introduce a el arreglo */
 
 let newarr = ["1", 1, 2, 3, "2"];
 document.getElementById("arreglo").innerHTML ="Before: " + JSON.stringify(newarr); 
 
-function myArreglo_usuario(arr, items){ //esta fucin toma los argumentos desde un arreglo y del usuariopara agregar y eliminar un datos.
+let other_array = ["1", 1, 2, 3, "2"]; 
+document.getElementById("pila_arreglo").innerHTML ="Before: " + JSON.stringify(other_array); 
 
-    arr.push(items); //introduce un nuevo dato al final del arreglo.
+
+
+
+function myArreglo_usuario(arr, item){ //esta fucin toma los argumentos desde un arreglo y del usuariopara agregar y eliminar un datos.
+
+    arr.push(item); //introduce un nuevo dato al final del arreglo.
     let delet= arr.shift();//elimina y almacena el dato de la posicion 0. 
     
 }
+
+
+function myArray_pila(arry, items){/**Esta funcion cunple el mismo procedimiento de la anterior, la diferencia es que en lugar de eliminar el primer dato
+    * elimina el ultimo dato de dicha funcion.
+    */
+
+    arry.unshift(items);
+    let borrar = arry.pop();
+}
+
+
+
 
 
 /** Esta funcion se encarga de tomar los datos del usuario, ejecutar la funcion myarreglo_usuario()
@@ -25,9 +41,38 @@ function dato_usr(){
 }
 
 
+function datos_pila(){
+    let get_user_pila = document.getElementById("pila_date").value;
+    myArray_pila(other_array, get_user_pila);
+    document.getElementById("arregloPila").innerHTML = "After: " + JSON.stringify(other_array); 
+    document.getElementById("pila_date").value = "";
+    
+
+}
+
+
+
+
+/**Esta funcion lo que hace es el uso de shift(), elimina el ultimo elemento de un 
+ * arreglo sin agregar un nuevo dato.
+ */
+function delete_shift(){
+
+    other_array.shift();
+    document.getElementById("arregloPila").innerHTML ="After: " + JSON.stringify(other_array); 
+
+
+}
+
+
+
+
 /** Funcion de limpiesa mef¿diante boton extra
 function limpiar(){
 
     document.getElementById("date_user").value = "";
 }
 */
+
+
+
